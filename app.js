c740459629eb8ce8e781bc26f6a6a1ac9e1f6c56
cfarrell <cfarrell@Deloitte.co.uk>
2020-01-29 11:52:32 +0000

    Commit contains following changes: Minor changes to the styles.css Form added to return id of selected role. Returns that role from an array

--- app.js
+++ app.js
@@ -16,46 +16,90 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/home.html");
 });
 
-// app.post("/", function(req,res){
-//
-//   var projectName = req.body.projectName;
-//   var client = req.body.client;
-//   var roleName = req.body.roleName;
-//   var grade = req.body.grade;
-//   var roleDescription = "Example Description";
-//   var location = "Example Location";
-//   var startDate = req.body.startDate;
-//   var endDate = "31 Dec 2020";
-//   var resourceManagerName = "Example Resource Manager";
-//   var resourceManagerEmail = "resource-manager@email.com";
-//
-//   res.render("project", {
-//     projectName: projectName,
-//     client: client,
-//     roleName: roleName,
-//     grade: grade,
-//     roleDescription: roleDescription,
-//     location: location,
-//     startDate: startDate,
-//     endDate: endDate,
-//     resourceManagerName: resourceManagerName,
-//     resourceManagerEmail: resourceManagerEmail
-//   });
-// });
+app.post("/", function(req, res) {
 
-app.get("/project", function(req, res) {
-  var projectName = "Example Project";
-  var client = "Example Client";
-  var roleName = "Example Role";
-  var grade = "Example Grade";
-  var roleDescription = "Example Description";
-  var location = "Example Location";
-  var startDate = "01 Jan 2020";
-  var endDate = "31 Dec 2020";
-  var resourceManagerName = "Example Resource Manager";
-  var resourceManagerEmail = "resource-manager@email.com";
+  var projectRoleId = Number(req.body.projectRoleID);
+
+  var exampleRoleArray = [{
+      projectName: "Project Martyr",
+      client: "Public Sector",
+      roleName: "PMO and RAID management",
+      grade: "Senior Consultant",
+      roleDescription: "It's a role!",
+      location: "London",
+      startDate: "13 Jan 2020",
+      endDate: "31 Dec 2020",
+      resourceManagerName: "Example Resource Manager",
+      resourceManagerEmail: "resource-manager@email.com"
+    },
+    {
+      projectName: "Project Martyr",
+      client: "Public Sector",
+      roleName: "NFT Manager",
+      grade: "Senior Consultant",
+      roleDescription: "It's a role again!",
+      location: "Gibraltar",
+      startDate: "19 Jan 2020",
+      endDate: "31 Dec 2020",
+      resourceManagerName: "Example Resource Manager",
+      resourceManagerEmail: "resource-manager@email.com"
+    }, {
+      projectName: "Project Martyr",
+      client: "Public Sector",
+      roleName: "NFT Analyst",
+      grade: "Analyst",
+      roleDescription: "It's a role!",
+      location: "Gibraltar",
+      startDate: "19 Jan 2020",
+      endDate: "31 Dec 2020",
+      resourceManagerName: "Example Resource Manager",
+      resourceManagerEmail: "resource-manager@email.com"
+    }
+  ];
+
+  switch (projectRoleId) {
+    case 1:
+      var projectName = exampleRoleArray[projectRoleId - 1].projectName;
+      var client = exampleRoleArray[projectRoleId - 1].client;
+      var roleName = exampleRoleArray[projectRoleId - 1].roleName;
+      var grade = exampleRoleArray[projectRoleId - 1].grade;
+      var roleDescription = exampleRoleArray[projectRoleId - 1].roleDescription;
+      var location = exampleRoleArray[projectRoleId - 1].location;
+      var startDate = exampleRoleArray[projectRoleId - 1].startDate;
+      var endDate = exampleRoleArray[projectRoleId - 1].endDate;
+      var resourceManagerName = exampleRoleArray[projectRoleId - 1].resourceManagerName;
+      var resourceManagerEmail = exampleRoleArray[projectRoleId - 1].resourceManagerEmail;
+      break;
+    case 2:
+      var projectName = exampleRoleArray[projectRoleId - 1].projectName;
+      var client = exampleRoleArray[projectRoleId - 1].client;
+      var roleName = exampleRoleArray[projectRoleId - 1].roleName;
+      var grade = exampleRoleArray[projectRoleId - 1].grade;
+      var roleDescription = exampleRoleArray[projectRoleId - 1].roleDescription;
+      var location = exampleRoleArray[projectRoleId - 1].location;
+      var startDate = exampleRoleArray[projectRoleId - 1].startDate;
+      var endDate = exampleRoleArray[projectRoleId - 1].endDate;
+      var resourceManagerName = exampleRoleArray[projectRoleId - 1].resourceManagerName;
+      var resourceManagerEmail = exampleRoleArray[projectRoleId - 1].resourceManagerEmail;
+      break;
+    case 3:
+      var projectName = exampleRoleArray[projectRoleId - 1].projectName;
+      var client = exampleRoleArray[projectRoleId - 1].client;
+      var roleName = exampleRoleArray[projectRoleId - 1].roleName;
+      var grade = exampleRoleArray[projectRoleId - 1].grade;
+      var roleDescription = exampleRoleArray[projectRoleId - 1].roleDescription;
+      var location = exampleRoleArray[projectRoleId - 1].location;
+      var startDate = exampleRoleArray[projectRoleId - 1].startDate;
+      var endDate = exampleRoleArray[projectRoleId - 1].endDate;
+      var resourceManagerName = exampleRoleArray[projectRoleId - 1].resourceManagerName;
+      var resourceManagerEmail = exampleRoleArray[projectRoleId - 1].resourceManagerEmail;
+      break;
+    default:
+      res.redirect("/failure");
+  }
 
   res.render("project", {
+
     projectName: projectName,
     client: client,
     roleName: roleName,
@@ -69,10 +113,10 @@ app.get("/project", function(req, res) {
   });
 });
 
-// // Return To Home Page if Errors Occur
-// app.post("/failure", function(req, res) {
-//   res.redirect("/");
-// });
+// Return To Home Page if Errors Occur
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
