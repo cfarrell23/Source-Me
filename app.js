223a4e0b2b4d5472c925ac94cdde838d5158c16d
cfarrell <cfarrell@Deloitte.co.uk>
2020-01-29 01:35:08 +0000

    Commit contains the following changes: Switched to project.ejs from .html Able to identify clicked record's ID

--- app.js
+++ app.js
@@ -16,8 +16,57 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/home.html");
 });
 
+// app.post("/", function(req,res){
+//
+//   var projectName = req.body.projectName;
+//   var client = req.body.client;
+//   var roleName = req.body.roleName;
+//   var grade = req.body.grade;
+//   var roleDescription = "Example Description";
+//   var location = "Example Location";
+//   var startDate = req.body.startDate;
+//   var endDate = "31 Dec 2020";
+//   var resourceManagerName = "Example Resource Manager";
+//   var resourceManagerEmail = "resource-manager@email.com";
+//
+//   res.render("project", {
+//     projectName: projectName,
+//     client: client,
+//     roleName: roleName,
+//     grade: grade,
+//     roleDescription: roleDescription,
+//     location: location,
+//     startDate: startDate,
+//     endDate: endDate,
+//     resourceManagerName: resourceManagerName,
+//     resourceManagerEmail: resourceManagerEmail
+//   });
+// });
+
 app.get("/project", function(req, res) {
-  res.sendFile(__dirname + "/project.html");
+  var projectName = "Example Project";
+  var client = "Example Client";
+  var roleName = "Example Role";
+  var grade = "Example Grade";
+  var roleDescription = "Example Description";
+  var location = "Example Location";
+  var startDate = "01 Jan 2020";
+  var endDate = "31 Dec 2020";
+  var resourceManagerName = "Example Resource Manager";
+  var resourceManagerEmail = "resource-manager@email.com";
+
+  res.render("project", {
+    projectName: projectName,
+    client: client,
+    roleName: roleName,
+    grade: grade,
+    roleDescription: roleDescription,
+    location: location,
+    startDate: startDate,
+    endDate: endDate,
+    resourceManagerName: resourceManagerName,
+    resourceManagerEmail: resourceManagerEmail
+  });
 });
 
 // // Return To Home Page if Errors Occur
