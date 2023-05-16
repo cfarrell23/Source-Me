a504153721c5601792b1099b5a0e8f6164e8676f
Caoilfhinn Farrell <cfarrell@deloitte.co.uk>
2023-05-16 14:11:37 +0100

    Upgrade packages to account for vulnerabilities

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ const ejs = require("ejs");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/home.html");
 });
 
-app.post("/", function(req, res) {
-
+app.post("/", function (req, res) {
   var projectRoleId = Number(req.body.projectRoleID);
 
-  var exampleRoleArray = [{
+  var exampleRoleArray = [
+    {
       projectName: "Project Martyr",
       client: "Public Sector",
       roleName: "PMO and RAID management",
@@ -30,7 +32,7 @@ app.post("/", function(req, res) {
       startDate: "13 Jan 2020",
       endDate: "31 Dec 2020",
       resourceManagerName: "Example Resource Manager",
-      resourceManagerEmail: "resource-manager@email.com"
+      resourceManagerEmail: "resource-manager@email.com",
     },
     {
       projectName: "Project Martyr",
@@ -42,8 +44,9 @@ app.post("/", function(req, res) {
       startDate: "19 Jan 2020",
       endDate: "31 Dec 2020",
       resourceManagerName: "Example Resource Manager",
-      resourceManagerEmail: "resource-manager@email.com"
-    }, {
+      resourceManagerEmail: "resource-manager@email.com",
+    },
+    {
       projectName: "Project Martyr",
       client: "Public Sector",
       roleName: "NFT Analyst",
@@ -53,8 +56,8 @@ app.post("/", function(req, res) {
       startDate: "19 Jan 2020",
       endDate: "31 Dec 2020",
       resourceManagerName: "Example Resource Manager",
-      resourceManagerEmail: "resource-manager@email.com"
-    }
+      resourceManagerEmail: "resource-manager@email.com",
+    },
   ];
 
   switch (projectRoleId) {
@@ -67,8 +70,10 @@ app.post("/", function(req, res) {
       var location = exampleRoleArray[projectRoleId - 1].location;
       var startDate = exampleRoleArray[projectRoleId - 1].startDate;
       var endDate = exampleRoleArray[projectRoleId - 1].endDate;
-      var resourceManagerName = exampleRoleArray[projectRoleId - 1].resourceManagerName;
-      var resourceManagerEmail = exampleRoleArray[projectRoleId - 1].resourceManagerEmail;
+      var resourceManagerName =
+        exampleRoleArray[projectRoleId - 1].resourceManagerName;
+      var resourceManagerEmail =
+        exampleRoleArray[projectRoleId - 1].resourceManagerEmail;
       break;
     case 2:
       var projectName = exampleRoleArray[projectRoleId - 1].projectName;
@@ -79,8 +84,10 @@ app.post("/", function(req, res) {
       var location = exampleRoleArray[projectRoleId - 1].location;
       var startDate = exampleRoleArray[projectRoleId - 1].startDate;
       var endDate = exampleRoleArray[projectRoleId - 1].endDate;
-      var resourceManagerName = exampleRoleArray[projectRoleId - 1].resourceManagerName;
-      var resourceManagerEmail = exampleRoleArray[projectRoleId - 1].resourceManagerEmail;
+      var resourceManagerName =
+        exampleRoleArray[projectRoleId - 1].resourceManagerName;
+      var resourceManagerEmail =
+        exampleRoleArray[projectRoleId - 1].resourceManagerEmail;
       break;
     case 3:
       var projectName = exampleRoleArray[projectRoleId - 1].projectName;
@@ -91,15 +98,16 @@ app.post("/", function(req, res) {
       var location = exampleRoleArray[projectRoleId - 1].location;
       var startDate = exampleRoleArray[projectRoleId - 1].startDate;
       var endDate = exampleRoleArray[projectRoleId - 1].endDate;
-      var resourceManagerName = exampleRoleArray[projectRoleId - 1].resourceManagerName;
-      var resourceManagerEmail = exampleRoleArray[projectRoleId - 1].resourceManagerEmail;
+      var resourceManagerName =
+        exampleRoleArray[projectRoleId - 1].resourceManagerName;
+      var resourceManagerEmail =
+        exampleRoleArray[projectRoleId - 1].resourceManagerEmail;
       break;
     default:
       res.redirect("/failure");
   }
 
   res.render("project", {
-
     projectName: projectName,
     client: client,
     roleName: roleName,
@@ -109,15 +117,15 @@ app.post("/", function(req, res) {
     startDate: startDate,
     endDate: endDate,
     resourceManagerName: resourceManagerName,
-    resourceManagerEmail: resourceManagerEmail
+    resourceManagerEmail: resourceManagerEmail,
   });
 });
 
 // Return To Home Page if Errors Occur
-app.post("/failure", function(req, res) {
+app.post("/failure", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3005, function () {
+  console.log("Server started on port 3005");
 });
